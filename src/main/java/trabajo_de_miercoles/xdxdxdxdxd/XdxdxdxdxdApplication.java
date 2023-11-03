b73837df5e94bdf55f37a287332baa7f72fb244f
LAB_C_REAL <labc@continental.edu.pe>
2023-11-03 18:40:11 -0500

    version

---
+++ src/main/java/trabajo_de_miercoles/xdxdxdxdxd/XdxdxdxdxdApplication.java
@@ -0,0 +1,13 @@
+package trabajo_de_miercoles.xdxdxdxdxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdxdxdxdxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XdxdxdxdxdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trabajo_de_miercoles/xdxdxdxdxd/XdxdxdxdxdApplicationTests.java
@@ -0,0 +1,13 @@
+package trabajo_de_miercoles.xdxdxdxdxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdxdxdxdxdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
